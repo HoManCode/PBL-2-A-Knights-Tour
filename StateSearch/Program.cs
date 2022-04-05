3f6974290fc9dd75ac43333465601ef6c63f48f7
cjar95 <101533345+cjar95@users.noreply.github.com>
2022-04-05 10:07:41 +1000

    Add files via upload

--- StateSearch/Program.cs
+++ StateSearch/Program.cs
@@ -1,68 +1,105 @@
-﻿
-//https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/
-namespace knightTour
+﻿using System;
+
+namespace KnightsTour
 {
-    class knightTour
+    namespace knightTour
     {
-        static int [] xMove = {2,1,-1,-2,-2,-1,1,2};
-        static int [] yMove = {1,2,2,1,-1,-2,-2,-1};
-
-        static void Main(String[] args)
+        class knightTour
         {
-            Console.WriteLine("Please enter the size of the chess board: ");
-            int board_size = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the current position of the knight: ");
-            Console.WriteLine("x: ");
-            int row = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("y: ");
-            int col = Int32.Parse(Console.ReadLine());
-            
-            int move = 0;
-            int[,] board = new int [board_size,board_size];
+            //Represent the Knight's next move
+            //x = x coordinate
+            //y= y coordinate 
+            static int[] xMove = { 2, 1, -1, -2, -2, -1, 1, 2 };
+            static int[] yMove = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
-            for(int i = 0; i < board_size;i++)
+            static void Main(String[] args)
             {
-                for (int j = 0; j < board_size; j++)
+                int board_size;
+                int row;
+                int column;
+
+                //Prompts the user for a chessboard size continually until an acceptable size is chosen 
+                Start:
+                Console.WriteLine("Please enter the size of the chess board: ");
+                try
                 {
-                    board[i,j] = 0;
+                    board_size = Int32.Parse(Console.ReadLine());
                 }
+                catch
+                {
+                    Console.WriteLine("You have entered an incorrect input format");
+                    goto Start;
+                }
+                
+                //Prompts the user for the Knight's current position continually until an acceptable position is chosen
+                CoordinateValidation:
+                try
+                {
+                    Console.WriteLine("Please enter the current position of the knight: ");
+                    Console.WriteLine("x: ");
+                     row = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("y: ");
+                    column = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("You have entered an incorrect input format");
+                    goto CoordinateValidation;
+                }
+              
+                
+                //Creates an array from the user's indicated board size 
+                int[,] board = new int[board_size, board_size];
+                int move = 0;
+
+                for (int i = 0; i < board_size; i++)
+                {
+                    for (int j = 0; j < board_size; j++)
+                    {
+                        board[i, j] = 0;
+                    }
+                }
+
+                solve(board, row, column, move);
+                printBoard(board);
+            }
+            //Ensures move occuring is valid 
+            static bool moveValidation(int[,] board, int row, int column)
+            {
+                int board_size = board.GetLength(0);
+                return (row < board_size && row >= 0 && column < board_size && column >= 0 && board[row, column] == 0);
             }
             
-            solve(board, row, col, move);
-            printBoard(board);
-        }
-        static bool moveValidation (int [,]board, int row, int col )
-        {
-            int board_size = board.GetLength(0);
-            return (row < board_size && row >= 0 && col < board_size && col >=0 && board[row, col] == 0);
-        }
-        static bool solve(int[,]board, int row, int col, int move)
-        {
-            int board_size = board.GetLength(0);
-            if (move == board_size*board_size) return true;
-            for (int i = 0; i < board_size; i++)
+            //Recursive utility function to solve KT
+            static bool solve(int[,] board, int row, int column, int move)
             {
-                int new_x = row + xMove[i];
-                int new_y = col + yMove[i];
-                if (moveValidation(board, new_x, new_y))
+                int board_size = board.GetLength(0);
+                if (move == board_size * board_size) return true;
+                for (int i = 0; i < board_size; i++)
                 {
-                    board[new_x,new_y] = move;
-                    if (solve(board, new_x, new_y, move+1)) return true;
-                    else board[new_x, new_y] = 0;
+                    int new_x = row + xMove[i];
+                    int new_y = column + yMove[i];
+                    if (moveValidation(board, new_x, new_y))
+                    {
+                        board[new_x, new_y] = move;
+                        if (solve(board, new_x, new_y, move + 1)) return true;
+                        else board[new_x, new_y] = 0;
+                    }
                 }
+                return false;
             }
-            return false;
-        }
 
-        static void printBoard(int[,] board)
-        {
-            for (int i = 0; i < board.GetLength(0); i++)
+            //Prints the resultant board to the console
+            static void printBoard(int[,] board)
             {
-                for (int j = 0; j < board.GetLength(0); j++)
+                for (int i = 0; i < board.GetLength(0); i++)
                 {
-                    Console.Write(board[i,j] + " ");
+                    for (int j = 0; j < board.GetLength(0); j++)
+                    {
+                        Console.Write(board[i, j] + " ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
         }
     }
